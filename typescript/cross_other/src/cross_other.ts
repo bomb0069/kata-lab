5adcea249747f427e660f51d9d46d84f05cb3378
karan sivarat <bomb0069@gmail.com>
2020-04-02 23:44:08 +0700

    [CHANGED]: filter with compare inner/outer index into splice teamp array before reduce

--- typescript/cross_other/src/cross_other.ts
+++ typescript/cross_other/src/cross_other.ts
@@ -1,12 +1,10 @@
 export const crossOther = (numbers: number[]): number[] => {
-  const newNumbers = numbers.map((_, index) => {
-    return [...numbers]
-      .filter((_, innerIndex) => {
-        return innerIndex !== index
-      })
-      .reduce((multiply, element) => {
-        return multiply * element
-      }, 1)
+  const newNumbers = numbers.map((_, index, array) => {
+    const tempArray = [...array]
+    tempArray.splice(index, 1)
+    return tempArray.reduce((multiply, element) => {
+      return multiply * element
+    }, 1)
   })
   return newNumbers
 }
