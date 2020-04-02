15605766f4f8439f82b983f3ad4e7271a5a15392
karan sivarat <bomb0069@gmail.com>
2020-04-03 00:00:39 +0700

    [ROLLBACK]: to use filter to show my intent and fix index before use in inner loop

--- typescript/cross_other/src/cross_other.ts
+++ typescript/cross_other/src/cross_other.ts
@@ -1,10 +1,13 @@
 export const crossOther = (numbers: number[]): number[] => {
   const newNumbers = numbers.map((_, index, array) => {
-    const tempArray = [...array]
-    tempArray.splice(index, 1)
-    return tempArray.reduce((multiply, element) => {
-      return multiply * element
-    }, 1)
+    const outerIndex = index
+    return array
+      .filter((_, innerIndex) => {
+        return innerIndex !== outerIndex
+      })
+      .reduce((multiply, element) => {
+        return multiply * element
+      }, 1)
   })
   return newNumbers
 }
