4cb774046fb9bcc82f77726188c531163619aa18
karan sivarat <bomb0069@gmail.com>
2020-04-02 21:27:21 +0700

    [ADDED]: Only one member array will Return Array of 1 and two member array will Return swap member

--- typescript/cross_other/src/cross_other.ts
+++ typescript/cross_other/src/cross_other.ts
@@ -1,3 +1,8 @@
 export const crossOther = (numbers: number[]): number[] => {
+  if (numbers.length === 1) {
+    return [1]
+  } else if (numbers.length === 2) {
+    return [numbers[1], numbers[0]]
+  }
   return numbers
 }
--- typescript/cross_other/tests/cross_other.test.ts
+++ typescript/cross_other/tests/cross_other.test.ts
@@ -3,8 +3,26 @@ import { crossOther } from '../src/cross_other'
 describe('Cross value in other member in input array', () => {
   it('Empty Array will Return Empty Array',
     () => {
-      const expected: number[] = []
       const input: number[] = []
+      const expected: number[] = []
+
+      const actual = crossOther(input)
+      expect(actual).toEqual(expected)
+    })
+
+  it('Only one member array will Return Array of 1',
+    () => {
+      const input: number[] = [5]
+      const expected: number[] = [1]
+
+      const actual = crossOther(input)
+      expect(actual).toEqual(expected)
+    })
+
+  it('two member array will Return swap member',
+    () => {
+      const input: number[] = [5, 1]
+      const expected: number[] = [1, 5]
 
       const actual = crossOther(input)
       expect(actual).toEqual(expected)
