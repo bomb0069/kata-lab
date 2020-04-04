952606dadd3f1c3fd685e212d6e4ef1e104c7fb4
karan sivarat <bomb0069@gmail.com>
2020-04-04 21:35:53 +0700

    [REFACTOR]: from idea of YIM tansinee

--- typescript/cross_other/src/cross_other.ts
+++ typescript/cross_other/src/cross_other.ts
@@ -1,14 +1,7 @@
 export const crossOther = (numbers: number[]): number[] => {
-  const allCross = numbers.filter((element) => element !== 0)
-    .reduce((multiply, element) => multiply * element, 1)
-
-  const newNumbers = numbers.map((element, _, array) => {
-    const someElementIsZero = array.some((element) => element === 0)
-    if (someElementIsZero) {
-      return (element === 0) ? allCross : 0
-    } else {
-      return allCross / element
-    }
-  })
-  return newNumbers
+  return numbers.map(
+    (_, index, array) => array
+      .map((number, innerIdx) => index === innerIdx ? 1 : number)
+      .reduce((cross, element) => cross * element, 1)
+  )
 }
